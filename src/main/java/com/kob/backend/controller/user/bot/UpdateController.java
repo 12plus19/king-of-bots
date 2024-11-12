ee0b655cd32b2c2f8dee78a2a6a49f84887317db
liujiapeng678 <jj0723938@gmail.com>
2024-11-12 23:21:11 +0800

    改pojo的bot的时区，修改updateController的一个错误

--- src/main/java/com/kob/backend/controller/user/bot/UpdateController.java
+++ src/main/java/com/kob/backend/controller/user/bot/UpdateController.java
@@ -3,7 +3,7 @@ package com.kob.backend.controller.user.bot;
 import com.kob.backend.service.user.bot.UpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -13,7 +13,7 @@ public class UpdateController {
     @Autowired
     private UpdateService updateService;
     @PostMapping("/user/bot/update/")
-    public Map<String, String> update(@RequestBody Map<String, String> data) {
+    public Map<String, String> update(@RequestParam Map<String, String> data) {
         return updateService.update(data);
     }
 }
