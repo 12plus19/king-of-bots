a42c2b2199274fb05bc71eb314a11ec2f68abf12
liujiapeng678 <jj0723938@gmail.com>
2024-11-30 20:13:10 +0800

    对战列表分页

---
+++ backend/src/main/java/com/kob/backend/controller/record/GetRecordListController.java
@@ -0,0 +1,20 @@
+package com.kob.backend.controller.record;
+
+import com.alibaba.fastjson.JSONObject;
+import com.kob.backend.service.record.GetRecordListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class GetRecordListController {
+    @Autowired
+    private GetRecordListService getRecordListService;
+    @GetMapping("/record/getlist/")
+    public JSONObject getRecordList(Map<String, String> data) {
+        Integer pageNo = Integer.parseInt(data.get("page_no"));
+        return getRecordListService.getRecordList(pageNo);
+    }
+}
