ef6aef70e56000dc7614e5d4cd2a1b81d5321271
liujiapeng678 <jj0723938@gmail.com>
2024-12-02 13:23:35 +0800

    获取排行榜

--- backend/src/main/java/com/kob/backend/service/impl/user/account/LoginServiceImpl.java
+++ backend/src/main/java/com/kob/backend/service/impl/user/account/LoginServiceImpl.java
@@ -6,7 +6,6 @@ import com.kob.backend.service.user.account.LoginService;
 import com.kob.backend.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
